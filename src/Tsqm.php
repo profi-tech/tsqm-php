e0f3165739a50ca57a7ec68c7215e12e0646a1e6
Emin Chernov <chernovev@team.profi.ru>
2023-11-23 15:26:33 +0300

    Fix method comment

--- src/Tsqm.php
+++ src/Tsqm.php
@@ -8,7 +8,6 @@ use Generator;
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
 use Tsqm\Errors\ThrowMe;
-use Tsqm\Errors\TsqmError;
 use Tsqm\Errors\InvalidGenerator;
 use Tsqm\Errors\StopTheRun;
 use Tsqm\Errors\RunNotFound;
@@ -153,7 +152,7 @@ class Tsqm
     }
 
     /**
-     * Return all the scheduled run ids which is not started of finished
+     * Return all the scheduled run ids which is not finished
      * @param DateTime $until 
      * @param int $limit 
      * @return string[]
