58a2e2fe8e40e935b490900a20bc616c08e932c3
Emin Chernov <chernovev@team.profi.ru>
2023-11-30 13:05:42 +0300

    Fix pdo error handling

--- src/Events/EventRepository.php
+++ src/Events/EventRepository.php
@@ -6,6 +6,7 @@ use DateTime;
 use Exception;
 use PDO;
 use Tsqm\Errors\RepositoryError;
+use Tsqm\Helpers\PdoHelper;
 use Tsqm\Helpers\SerializationHelper;
 
 class EventRepository implements EventRepositoryInterface
@@ -31,7 +32,12 @@ class EventRepository implements EventRepositoryInterface
             $res = $this->pdo->prepare("
                 INSERT INTO events (run_id, ts, type, task_id, payload, hash) 
                 VALUES(:run_id, :ts, :type, :task_id, :payload, :hash)
-            ")->execute([
+            ");
+            if (!$res) {
+                throw new Exception(PdoHelper::formatErrorInfo($this->pdo->errorInfo()));
+            }
+
+            $res->execute([
                 'run_id' => $event->getRunId(),
                 'ts' => $event->getTs()->format('Y-m-d H:i:s.u'),
                 'type' => $event->getType(),
@@ -40,13 +46,9 @@ class EventRepository implements EventRepositoryInterface
                 'hash' => $event->getHash($salt),
             ]);
 
-            if (!$res) {
-                throw new RepositoryError("Failed to add event: execute() returned false");
-            }
-
             $id = $this->pdo->lastInsertId();
             if (!$id) {
-                throw new RepositoryError("Failed to add event: lastInsertId() returned 0");
+                throw new Exception("Failed to add event: lastInsertId() returned 0");
             }
 
             return $event->withId($id);
@@ -58,22 +60,22 @@ class EventRepository implements EventRepositoryInterface
     public function getCompletionEvent(string $runId, string $taskId): ?Event
     {
         try {
-            $st = $this->pdo->prepare("
-                SELECT * FROM events 
+            $res = $this->pdo->prepare("
+                SELECT * FROM events
                 WHERE run_id=:run_id AND task_id=:task_id AND type in (:type1, :type2)
                 ORDER BY id DESC LIMIT 1
              ");
-            $res = $st->execute([
+            if (!$res) {
+                throw new Exception(PdoHelper::formatErrorInfo($this->pdo->errorInfo()));
+            }
+            $res->execute([
                 'run_id' => $runId,
                 'task_id' => $taskId,
                 'type1' => Event::TYPE_TASK_COMPLETED,
                 'type2' => Event::TYPE_TASK_CRASHED,
             ]);
-            if (!$res) {
-                throw new RepositoryError("Failed to get completion event: execute() returned false");
-            }
 
-            $data = $st->fetch(PDO::FETCH_ASSOC);
+            $data = $res->fetch(PDO::FETCH_ASSOC);
             return $data ? Event::fromArray($data) : null;
         } catch (Exception $e) {
             throw new RepositoryError("Failed to get completed event: " . $e->getMessage(), 0, $e);
@@ -83,18 +85,19 @@ class EventRepository implements EventRepositoryInterface
     public function getStartedEvents(string $runId): array
     {
         try {
-            $st = $this->pdo->prepare("
+            $res = $this->pdo->prepare("
                 SELECT * FROM events
                 WHERE run_id = :run_id and type = :type
                 ORDER BY id 
             ");
-            $res = $st->execute(['run_id' => $runId, 'type' => Event::TYPE_TASK_STARTED]);
             if (!$res) {
-                throw new RepositoryError("Failed to get started events: execute() returned false");
+                throw new Exception(PdoHelper::formatErrorInfo($this->pdo->errorInfo()));
             }
 
+            $res->execute(['run_id' => $runId, 'type' => Event::TYPE_TASK_STARTED]);
+
             $rows = [];
-            while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+            while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
                 $rows[] = Event::fromArray($row);
             }
 
@@ -107,18 +110,19 @@ class EventRepository implements EventRepositoryInterface
     public function getFailedEvents(string $runId, string $taskId)
     {
         try {
-            $st = $this->pdo->prepare("
+            $res = $this->pdo->prepare("
                 SELECT * FROM events
                 WHERE run_id = :run_id and task_id = :task_id and type = :type
                 ORDER BY id 
             ");
-            $res = $st->execute(['run_id' => $runId, 'task_id' => $taskId, 'type' => Event::TYPE_TASK_FAILED]);
             if (!$res) {
-                throw new RepositoryError("Failed to get failed events: execute() returned false");
+                throw new Exception(PdoHelper::formatErrorInfo($this->pdo->errorInfo()));
             }
 
+            $res->execute(['run_id' => $runId, 'task_id' => $taskId, 'type' => Event::TYPE_TASK_FAILED]);
+
             $rows = [];
-            while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+            while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
                 $rows[] = Event::fromArray($row);
             }
 
---
+++ src/Helpers/PdoHelper.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tsqm\Helpers;
+
+use Exception;
+use PDOException;
+
+class PdoHelper
+{
+    public static function formatErrorInfo(array $errinfo): string
+    {
+        $err = $errinfo[0];
+        if (isset($errinfo[1])) $err .= " ({$errinfo[1]})";
+        if (isset($errinfo[2])) $err .= ": {$errinfo[2]}";
+        return "$err";
+    }
+}
