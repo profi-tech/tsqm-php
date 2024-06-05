c0b8edb393af540761fb998da7cb429c3e75805c
Emin Chernov <chernovev@team.profi.ru>
2024-06-05 07:48:10 +0000

    chore: Reformat sql scripts

--- db/sqlite_init.sql
+++ db/sqlite_init.sql
@@ -1,24 +1,24 @@
+CREATE TABLE `tsqm_tasks` (
+    `nid` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `id` VARCHAR(36) NOT NULL,
+    `parent_id` VARCHAR(36),
+    `root_id` VARCHAR(36) NOT NULL,
+    `created_at` TIMESTAMP(6) NOT NULL,
+    `scheduled_for` TIMESTAMP(6) NOT NULL,
+    `started_at` TIMESTAMP(6),
+    `finished_at` TIMESTAMP(6),
+    `is_secret` BOOLEAN NOT NULL DEFAULT false,
+    `name` VARCHAR(255) NOT NULL,
+    `args` BLOB,
+    `result` BLOB,
+    `error` BLOB,
+    `retry_policy` JSON,
+    `retried` SMALLINT NOT NULL DEFAULT 0,
+    UNIQUE (`id`)
+);
 
-CREATE TABLE
-    `tsqm_tasks` (
-        `nid` INTEGER PRIMARY KEY AUTOINCREMENT,
-        `id` VARCHAR(36) NOT NULL,
-        `parent_id` VARCHAR(36),
-        `root_id` VARCHAR(36) NOT NULL,
-        `created_at` TIMESTAMP(6) NOT NULL,
-        `scheduled_for` TIMESTAMP(6) NOT NULL,
-        `started_at` TIMESTAMP(6),
-        `finished_at` TIMESTAMP(6),
-        `is_secret` BOOLEAN NOT NULL DEFAULT false,
-        `name` VARCHAR(255) NOT NULL,
-        `args` BLOB,
-        `result` BLOB,
-        `error` BLOB,
-        `retry_policy` JSON,
-        `retried` SMALLINT NOT NULL DEFAULT 0
-    );
-
-CREATE UNIQUE INDEX `tsqm_tasks_idx_id` ON `tsqm_tasks` (`id`);
 CREATE INDEX `tsqm_tasks_idx_parent_id` ON `tsqm_tasks` (`parent_id`);
+
 CREATE INDEX `tsqm_tasks_idx_root_id` ON `tsqm_tasks` (`root_id`);
+
 CREATE INDEX `tsqm_tasks_idx_scheduled_for` ON `tsqm_tasks` (`scheduled_for`);
